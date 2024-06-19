69cd9652dd5ef1fd438347d2af49ee0be5bc3ccd
Lucas Albuquerque <Lucas Albuquerque>
2024-06-19 19:10:15 -0300

    Implementando Controlador de Autenticacao e seus metodos

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,33 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminProjectController;
 use App\Http\Controllers\AdminUserController;
 use App\Http\Controllers\AdminCategoryController;
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('login', [AuthenticationController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthenticationController::class, 'login']);
+Route::get('register', [AuthenticationController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [AuthenticationController::class, 'register']);
+Route::post('logout', [AuthenticationController::class, 'logout'])->name('logout');
+
+Route::get('forgot-password', [AuthenticationController::class, 'showForgotPasswordForm'])->name('password.request');
+Route::post('forgot-password', [AuthenticationController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('reset-password/{token}', [AuthenticationController::class, 'showResetPasswordForm'])->name('password.reset');
+Route::post('reset-password', [AuthenticationController::class, 'resetPassword'])->name('password.update');
+
+// Rotas para Usuários
+Route::middleware('auth')->group(function () {
+    Route::resource('/admin/users', AdminUserController::class)->names([
+        'index' => 'admin.users.index',
+        'create' => 'admin.users.create',
+        'store' => 'admin.users.store',
+        'edit' => 'admin.users.edit',
+        'update' => 'admin.users.update',
+        'destroy' => 'admin.users.destroy',
+    ]);
 });
 
 Route::resource('/admin/projetos', AdminProjectController::class)->names([
@@ -27,15 +51,6 @@ Route::post('/admin/projetos/{id}/add-image', [AdminProjectController::class, 'a
 Route::delete('/admin/projetos/{projectId}/delete-image/{imageId}', [AdminProjectController::class, 'deleteImage'])
     ->name('admin.projetos.deleteImage');
 
-// Rotas para Usuários
-Route::resource('/admin/users', AdminUserController::class)->names([
-    'index' => 'admin.users.index',
-    'create' => 'admin.users.create',
-    'store' => 'admin.users.store',
-    'edit' => 'admin.users.edit',
-    'update' => 'admin.users.update',
-    'destroy' => 'admin.users.destroy',
-]);
 
 // Rotas para Categorias
 Route::resource('/admin/categories', AdminCategoryController::class)->names([
