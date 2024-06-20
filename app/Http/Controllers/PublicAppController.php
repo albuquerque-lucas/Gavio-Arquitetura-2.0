3a7849f0f8169d6b34fee48d8610af86b1496663
Lucas Albuquerque <Lucas Albuquerque>
2024-06-20 16:39:00 -0300

    Implementando carrossel de imagens da pagina inicial

--- app/Http/Controllers/PublicAppController.php
+++ app/Http/Controllers/PublicAppController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class PublicAppController extends Controller
 {
     public function renderHomePage()
     {
         $title = 'Gávio Arquitetura e Interiores | Home';
-        return view('public.home-page', compact('title'));
+        $projects = Project::where('status', true)->get();
+
+        return view('public.home-page', compact('title', 'projects'));
     }
 }
--- resources/views/public/home-page.blade.php
+++ resources/views/public/home-page.blade.php
@@ -1,5 +1,39 @@
 @extends('public-layout')
 
+@section('extra-css')
+    @vite('resources/scss/public/home.scss')
+@endsection
+
 @section('content')
-    <h1>Pagina Inicial</h1>
+<main class="main container">
+    <section class='carousel_section container'>
+        <div id="carouselExampleIndicators" class="carousel slide">
+            <div class="carousel-indicators">
+                @foreach($projects as $index => $project)
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{ $index }}" class="{{ $index === 0 ? 'active' : '' }}"
+                        aria-current="true" aria-label="Slide {{ $index + 1 }}"></button>
+                @endforeach
+            </div>
+            <div class="carousel-inner">
+                @foreach($projects as $index => $project)
+                    <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
+                        <div class="d-flex justify-content-center">
+                            <img src="{{ $project->coverUrl() }}" class="d-block" alt="{{ $project->title }}">
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+</main>
 @endsection
