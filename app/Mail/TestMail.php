c7a8dbf44575561d303fcc951d3be0c4e9a277ba
Lucas Albuquerque <Lucas Albuquerque>
2024-06-20 09:52:41 -0300

    Corrigindo configuracoes de envio de email.

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -3,10 +3,7 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class TestMail extends Mailable
@@ -20,39 +17,10 @@ class TestMail extends Mailable
         $this->details = $details;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Test Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
-    }
-
     public function build()
     {
         return $this->subject('Test Email from Gavio Arquitetura')
-                    ->view('emails.test');
+                    ->view('emails.test')
+                    ->with('details', $this->details);
     }
 }
--- config/mail.php
+++ config/mail.php
@@ -47,6 +47,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'verify_peer' => false,
         ],
 
         'ses' => [
