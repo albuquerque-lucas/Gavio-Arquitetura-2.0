d0614d6db436cb243878656f1f50bb81c004d9ca
Lucas Albuquerque <Lucas Albuquerque>
2024-06-28 08:07:58 -0300

    Alterando caminho para icone da aba de navegacao

--- resources/views/admin-layout.blade.php
+++ resources/views/admin-layout.blade.php
@@ -10,7 +10,7 @@
     @vite('resources/js/app.js') --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="icon" href="{{ asset('public/storage/logo/gavioarquitetura-icone-02.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('storage/logo/gavioarquitetura-icone-02.png') }}" type="image/png">
 </head>
 
 <body class='bg-dark'>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js', 'resources/scss/public-app.scss', 'resources/js/public-app.js'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js', 'resources/scss/publicapp.scss', 'resources/js/public-app.js'],
             refresh: true,
         }),
     ],
