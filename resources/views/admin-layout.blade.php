07c020813fbde1fe3386a44081426efbe9a98629
Lucas Albuquerque <Lucas Albuquerque>
2024-08-05 16:38:00 -0300

    Convertendo scss para css

--- resources/views/admin-layout.blade.php
+++ resources/views/admin-layout.blade.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gavio Arquitetura | Admin | Projetos</title>
-    @vite('resources/scss/app.scss')
-    @vite('resources/js/app.js')
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    {{-- @vite('resources/scss/app.scss')
+    @vite('resources/js/app.js') --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="icon" href="{{ asset('storage/logo/gavioarquitetura-icone-02.png') }}" type="image/png">
 </head>
 
@@ -60,11 +60,11 @@
     </main>
     <footer class='layout-footer'>
     </footer>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/9aa910470c.js" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/app.js') }}"></script> --}}
-    @vite('resources/js/app.js')
+    <script src="{{ asset('js/app.js') }}"></script>
+    {{-- @vite('resources/js/app.js') --}}
 </body>
 
 </html>
