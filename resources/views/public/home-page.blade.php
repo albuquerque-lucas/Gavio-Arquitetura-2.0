f545e07c8a673c7c452b0a449fb4de05ab39cb2a
Lucas Albuquerque <Lucas Albuquerque>
2024-08-06 17:36:37 -0300

    Adicionando efeito js fade in na home page

--- resources/views/public/home-page.blade.php
+++ resources/views/public/home-page.blade.php
@@ -1,28 +1,28 @@
 @extends('home-layout')
 
 @section('content')
-<section class='home_section_container'>
-    <div class="d-flex home-item home-images">
+<section class='home_section_container fade-in'>
+    <div class="d-flex home-item home-images fade-in">
         <a href="{{ route('public.home') }}">
-            <img src="{{ asset('storage/logo/gavioarquitetura-icone-02.png') }}" alt="gavio-arquitetura-logo" class="home-logo">
+            <img src="{{ asset('storage/logo/gavioarquitetura-icone-02.png') }}" alt="gavio-arquitetura-logo" class="home-logo fade-in">
         </a>
         <a href="{{ route('public.home') }}">
-            <img src="{{ asset('storage/logo/gavioarquitetura-escrita-01.png') }}" alt="gavio-arquitetura-logo" class="home-logo">
+            <img src="{{ asset('storage/logo/gavioarquitetura-escrita-01.png') }}" alt="gavio-arquitetura-logo" class="home-logo fade-in">
         </a>
     </div>
-    <nav class="home-item home-nav">
+    <nav class="home-item home-nav fade-in">
         <ul>
-            <li>
+            <li class="fade-in">
                 <a href="{{ route('public.about.us') }}">
                     Sobre
                 </a>
             </li>
-            <li>
+            <li class="fade-in">
                 <a href="{{ route('public.projects', 'residencial') }}">
                     Projetos
                 </a>
             </li>
-            <li>
+            <li class="fade-in">
                 <a href="{{ route('public.contact.us') }}">
                     Contato
                 </a>
@@ -30,4 +30,15 @@
         </ul>
     </nav>
 </section>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+    const fadeElements = document.querySelectorAll('.fade-in');
+
+    fadeElements.forEach((element) => {
+        element.classList.add('show');
+    });
+});
+
+</script>
 @endsection
