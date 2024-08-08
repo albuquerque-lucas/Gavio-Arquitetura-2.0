1288f655fc93140b73d66d9afda2dbb50e56cd39
Lucas Albuquerque <Lucas Albuquerque>
2024-08-08 19:34:01 -0300

    .

--- resources/views/public-layout.blade.php
+++ resources/views/public-layout.blade.php
@@ -9,7 +9,7 @@
     @vite('resources/scss/publicapp.scss') --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/publicapp.css') }}">
-    <link rel="icon" href="{{ asset('storage/logo/gavioarquitetura-icone-02.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('storage/logo/gavioarquitetura-icone-white.png') }}" type="image/png">
 </head>
 <body class='body'>
     <header class="header">
