2dc6696aefff84df92e33ce42e138420c23a2332
Lucas Albuquerque <Lucas Albuquerque>
2024-08-08 00:17:35 -0300

    .

--- resources/views/public/project-show.blade.php
+++ resources/views/public/project-show.blade.php
@@ -5,7 +5,7 @@
     <div class="container mt-5">
         <div class="row mb-4">
             <div class="col-12 d-flex justify-content-between align-items-center">
-                <a href="{{ route('public.projects', $project->category->slug) }}" class="btn btn-secondary go-back-link">Voltar</a>
+                <a href="{{ route('public.projects', $project->category->slug) }}" class="btn btn-dark go-back-link">Voltar</a>
             </div>
         </div>
         {{-- <div class="row mb-4">
