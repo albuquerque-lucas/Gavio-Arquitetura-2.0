81827870c8f2f841b2c2d5e95f15aaba942d9442
Lucas Albuquerque <Lucas Albuquerque>
2024-08-07 22:44:37 -0300

    .

--- resources/views/home-layout.blade.php
+++ resources/views/home-layout.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    @vite('resources/scss/app.scss')
-    @vite('resources/scss/publicapp.scss')
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/publicapp.css') }}"> --}}
+    {{-- @vite('resources/scss/app.scss')
+    @vite('resources/scss/publicapp.scss') --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/publicapp.css') }}">
     <link rel="icon" href="{{ asset('storage/logo/gavioarquitetura-icone-02.png') }}" type="image/png">
 </head>
 <body class='body'>
